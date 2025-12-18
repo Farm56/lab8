6875c8870a7ae1e5f245e22298b303d467845c89
Farm56 <sstg3455@gmail.com>
2025-12-18 15:13:18 +0700

    complete_lab8_2.cpp

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,13 +1,48 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
 char findGrade(double score) {
-    if (score > 90) {
-        return 'A';
-    } else if (score > 75) {
-        return 'B';
-    } else if (score > 60) {
-        return 'C';
-    } else if (score > 45) {
-        return 'D';
-    } else {
-        return 'F';
+    if (score > 90) return 'A';
+    else if (score > 75) return 'B';
+    else if (score > 60) return 'C';
+    else if (score > 45) return 'D';
+    else return 'F';
+}
+
+int main(){
+    
+    int N, i = 0;
+    cout << "Enter the number of students: ";
+    cin >> N;
+    string name[N];
+    float score[N];
+
+   
+    while(i < N){
+        cout << "Name of student " << i+1 << ": ";
+        cin.ignore(); 
+        
+        getline(cin, name[i]);
+        
+        cout << "Score of student " << i+1 << ": ";
+        
+        cin >> score[i];
+        i++;
     }
+
+    
+    i = 0;
+    cout << "---------------------------------------------\n";
+    cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
+    cout << "---------------------------------------------\n";
+    while(i < N){
+        cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+        i++;
+    }
+    cout << "---------------------------------------------\n";
+
+    return 0;
 }
