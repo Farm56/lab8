24096c4c61eb1c57037c178cb92ec187012a0437
Farm56 <sstg3455@gmail.com>
2025-12-18 15:17:21 +0700

    complete_lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,47 +1,43 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
 using namespace std;
 
+int main() {
+    int age, height, bounty;
+    string character;
 
-char findGrade(double score) {
-    if (score > 90) return 'A';
-    else if (score > 75) return 'B';
-    else if (score > 60) return 'C';
-    else if (score > 45) return 'D';
-    else return 'F';
-}
+    cout << "Enter your age: ";
+    cin >> age;
 
-int main(){
-    
-    int N, i = 0;
-    cout << "Enter the number of students: ";
-    cin >> N;
-    string name[N];
-    float score[N];
-
-    
-    while(i < N){
-        cout << "Name of student " << i+1 << ": ";
-        cin.ignore();
-        getline(cin, name[i]);
-        
-        cout << "Score of student " << i+1 << ": ";
-        
-        cin >> score[i];
-        i++;
+    if (age <= 25) {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100) {
+            character = "Chopper";
+        } else if (height < 180) {
+            character = "Usopp";
+        } else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000) {
+                character = "Zoro";
+            } else {
+                character = "Sanji";
+            }
+        }
+    } else if (age <= 60) {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000) {
+            character = "Jinbe";
+        } else {
+            character = "Franky";
+        }
+    } else {
+        character = "Brook";
     }
 
-    
-    i = 0;
-    cout << "---------------------------------------------\n";
-    cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
-    cout << "---------------------------------------------\n";
-    while(i < N){
-        cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-        i++;
-    }
-    cout << "---------------------------------------------\n";
+    cout << "Your character = " << character << endl;
 
     return 0;
 }
